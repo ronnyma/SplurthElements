b278dc5aae39ab04e63c8a422f65cb1e7f70a36e
Ronny Mandal <ronnyma@transfinite.no>
2016-08-10 22:17:44 +0200

    Corrected comments

--- src/main/java/no/transfinite/Elements/SymbolGenerator.java
+++ src/main/java/no/transfinite/Elements/SymbolGenerator.java
@@ -7,13 +7,14 @@ public class SymbolGenerator
 {
     public String generateSymbol(String elementName)
     {
-        //Find the lowest ascii value from idx 0 to l-1
+
         char[] s = new char[2];
         s[0] = elementName.toLowerCase().charAt(0);
         s[1] = elementName.toLowerCase().charAt(elementName.length()-1);
         int idx = 0;
 
-        for(int i = 1;i < elementName.length()-1; i++)
+        //Find the lowest ascii value from 0 to l-1.
+        for(int i = 1; i < elementName.length()-1; i++)
         {
             if (elementName.toLowerCase().charAt(i) < s[0])
             {
@@ -21,8 +22,8 @@ public class SymbolGenerator
                 idx = ++i;
             }
         }
-
-        for(int i = idx;i < elementName.length(); i++)
+        //Find the lowest ascii value from idx to l, inclusive.
+        for(int i = idx; i < elementName.length(); i++)
         {
             if (elementName.toLowerCase().charAt(i) < s[1])
             {
