0fd137dfd294f6483a5725c9c21fa5cd6890d34e
Ronny Mandal <ronnyma@transfinite.no>
2016-08-10 22:16:30 +0200

    Calculate the number of valid symbols
    Working, poorly tested, not optimized.

---
+++ src/main/java/no/transfinite/Elements/CalculateNumberOfValidSymbols.java
@@ -0,0 +1,7 @@
+package no.transfinite.Elements;
+
+/**
+ * Created by ronnyma on 10/08/16.
+ */
+public class CalculateNumberOfValidSymbols {
+}
---
+++ src/test/java/no/transfinite/Elements/CalculateNumberOfValidSymbolsTest.java
@@ -0,0 +1,10 @@
+package no.transfinite.Elements;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ronnyma on 10/08/16.
+ */
+public class CalculateNumberOfValidSymbolsTest {
+
+}
